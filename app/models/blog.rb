4aff6cdad389f4dda5abcc84f60fc9dab6cb45e0
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-12 23:06:46 +0900

    added favorite function

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,8 @@
 class Blog < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+  
    mount_uploader :image, ImageUploader
 
    validates :image, presence: true
