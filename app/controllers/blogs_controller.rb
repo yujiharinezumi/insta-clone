91c3484ac5822acb84a16d28d57b12e30ec33cae
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2019-10-12 20:33:14 +0900

    can indivate user_name in index page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,7 +13,7 @@ class BlogsController < ApplicationController
   end
 
   def create
-    @blog = Blog.new(blog_params)
+    @blog = current_user.blogs.build(blog_params)
     if params[:back]
       render :new
     else
@@ -47,7 +47,7 @@ class BlogsController < ApplicationController
     end
 
     def confirm
-       @blog = Blog.new(blog_params)
+       @blog = current_user.blogs.build(blog_params)
        render :new if @blog.invalid?
     end
 
